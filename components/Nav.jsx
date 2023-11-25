df3a27c1036621a1ccd3d8cde649559432af21a2
master2it <mahdavi2it@gmail.com>
2023-11-25 18:27:24 +0300

    only show home icon in menu when user is not in home page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import { useState, useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -102,6 +103,7 @@ const Nav = () => {
 
 const Menu = ({ image, name }) => {
   const dropdown = useRef(null);
+  const pathName = usePathname();
 
   useEffect(() => {
     function handleClick(event) {
@@ -148,27 +150,29 @@ const Menu = ({ image, name }) => {
             Create Your Prompt
           </Link>
         </li>
-        <li className="flex md:hidden">
-          <Link href="/">
-            <svg
-              width="1.2rem"
-              height="1.2rem"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeWidth="1.5"
+        {pathName != "/" && (
+          <li className="flex md:hidden">
+            <Link href="/">
+              <svg
+                width="1.2rem"
+                height="1.2rem"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M9 16c.85.63 1.885 1 3 1s2.15-.37 3-1" />
-                <path d="m21.636 12.958l-.279 1.937c-.487 3.388-.731 5.081-1.906 6.093C18.276 22 16.553 22 13.106 22h-2.212c-3.447 0-5.17 0-6.345-1.012c-1.175-1.012-1.419-2.705-1.906-6.093l-.279-1.937c-.38-2.637-.57-3.956-.029-5.083c.54-1.127 1.691-1.813 3.992-3.183l1.385-.825C9.8 2.622 10.846 2 12 2c1.154 0 2.199.622 4.288 1.867l1.385.825c2.3 1.37 3.451 2.056 3.992 3.183" />
-              </g>
-            </svg>
-            Go Home
-          </Link>
-        </li>
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
+                >
+                  <path d="M9 16c.85.63 1.885 1 3 1s2.15-.37 3-1" />
+                  <path d="m21.636 12.958l-.279 1.937c-.487 3.388-.731 5.081-1.906 6.093C18.276 22 16.553 22 13.106 22h-2.212c-3.447 0-5.17 0-6.345-1.012c-1.175-1.012-1.419-2.705-1.906-6.093l-.279-1.937c-.38-2.637-.57-3.956-.029-5.083c.54-1.127 1.691-1.813 3.992-3.183l1.385-.825C9.8 2.622 10.846 2 12 2c1.154 0 2.199.622 4.288 1.867l1.385.825c2.3 1.37 3.451 2.056 3.992 3.183" />
+                </g>
+              </svg>
+              Go Home
+            </Link>
+          </li>
+        )}
         <li>
           <Link href="/profile">
             <svg
